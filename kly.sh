db4e821418c719c2f947ec448f768edb8bbbeef4
Kyle <81945767+Klyxen@users.noreply.github.com>
2025-04-10 17:11:32 +0800

    Update kly.sh

--- kly.sh
+++ kly.sh
@@ -1,7 +1,15 @@
+echo "installing klyxen..."
+sleep 2
 wget https://github.com/Klyxen/klyxen-repo/raw/main/klyxen -O klyxen
-
+echo "giving permissions..."
+sleep 1
 chmod +x klyxen
-
+echo "done"
+echo "moving..."
+sleep 2
 mv klyxen /usr/bin/
-
+echo "done"
+sleep 0.5
+echo "klyxen is ready."
+sleep 1
 sudo klyxen
