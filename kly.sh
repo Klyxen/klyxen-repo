743b4f21499c517675e726638cbe526d96030ebc
Kyle <81945767+Klyxen@users.noreply.github.com>
2025-04-10 17:30:55 +0800

    Update kl.sh

--- kly.sh
+++ kly.sh
@@ -1,17 +1,23 @@
-echo "Installing klyxen..."
-sleep 2
-wget https://github.com/Klyxen/klyxen-repo/raw/main/klyxen -O klyxen
-echo "Giving permissions..."
-sleep 1
-chmod +x klyxen
-echo "Done"
-echo "Moving..."
-sleep 2
-sudo mv klyxen /usr/bin/
-echo "Done"
-sleep 0.5
-echo "Klyxen is ready."
-sleep 0.5
-echo "Running..."
-sudo klyxen
-rm kly.sh
+file_path="/usr/bin/klyxen
+
+if [ -e "$file_path" ]; then
+  echo "File already exists!"
+else
+  echo "Installing klyxen..."
+  sleep 2
+  wget https://github.com/Klyxen/klyxen-repo/raw/main/klyxen -O klyxen
+  echo "Giving permissions..."
+  sleep 1
+  chmod +x klyxen
+  echo "Done"
+  echo "Moving..."
+  sleep 2
+  sudo mv klyxen /usr/bin/
+  echo "Done"
+  sleep 0.5
+  echo "Klyxen is ready."
+  sleep 0.5
+  echo "Running..."
+  sudo klyxen
+  rm kly.sh
+fi
